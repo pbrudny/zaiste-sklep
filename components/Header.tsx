ce086478c8621afaea315bbc3bb06b9ec7a56112
Piotr Brudny <pbrudny@gmail.com>
2022-06-16 15:27:17 +0200

    Add selected style to active link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
+import { useRouter} from "next/router";
+export const Header = () => {
+  const router = useRouter();
 
-export const Header = () =>
+  return (
   <header className="max-w-md mx-auto w-full">
     <nav className="bg-gray-700 px-4 py-2 text-white">
       <Link href="/">
-        <a>Główna</a>
+        <a className={router.pathname == "/" ? "selected" : ""}>Główna</a>
+      </Link>
+      <Link href="/users/ja">
+        <a className={router.pathname == "/users/ja" ? "selected" : ""}>Ja</a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a className={router.pathname == "/about" ? "selected" : ""}>About</a>
       </Link>
     </nav>
   </header>
+  )
+}
