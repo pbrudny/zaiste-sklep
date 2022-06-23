eab2d31296262fc1e3b288960bf6f5f12248a5c0
Piotr Brudny <pbrudny@gmail.com>
2022-06-23 12:06:30 +0200

    Use ReactQuery to show CSR for products

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
+    <QueryClientProvider client={client}>
       <Component {...pageProps} />
-      </>
+    </QueryClientProvider>
+
+  </>
 }
 
 export default MyApp
