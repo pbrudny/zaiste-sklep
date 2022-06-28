20a6faf74f753459bd03db9b791d32fed2009f01
Piotr Brudny <pbrudny@gmail.com>
2022-06-28 11:50:23 +0200

    Add CSR pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface PaginationProps {
   totalPages: number,
   current: number,
@@ -12,22 +14,32 @@ const Pagination = ({totalPages, current}: PaginationProps) => {
         {pages.map((page) => {
             if (page == current) {
               return (
-                <a
-                  href="#"
-                  className="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-                  aria-current="page"
+                <Link
+                  key={page}
+                  href={`/products-csr-paginated?page=${page}`}
                 >
-                  {page}
-                </a>
+                  <a
+                    className="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
+                    aria-current="page"
+                  >
+                    {page}
+                  </a>
+                </Link>
               )
             }
 
             return (
-              <a key={page}
-                 href="#"
-                 className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-              >{page}
-              </a>
+              <Link
+                key={page}
+                href={`/products-csr-paginated?page=${page}`}
+              >
+                <a
+                   href={`/products-csr-paginated?page=${page}`}
+                   className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
+                >
+                  {page}
+                </a>
+              </Link>
             )
           }
         )
