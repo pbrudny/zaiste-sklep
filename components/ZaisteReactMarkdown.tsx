89cb27f9ddfd7c7def0c4936be2cf4da47477da8
Piotr Brudny <pbrudny@gmail.com>
2022-07-07 15:19:11 +0200

    Do not wrap external links in Markdown

--- components/ZaisteReactMarkdown.tsx
+++ components/ZaisteReactMarkdown.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import {MDXRemote} from "next-mdx-remote";
 import {MarkdownResult} from "../utils";
 
+const isExternalURL = (url:string) => new URL(url).origin !== location.origin;
+
 const ZaisteReactMarkdown = ({ children }: { children: MarkdownResult }) => {
   return <MDXRemote
     {...children}
@@ -10,6 +12,11 @@ const ZaisteReactMarkdown = ({ children }: { children: MarkdownResult }) => {
       if (!href) {
         return <a {...props}></a>
       }
+
+      if (isExternalURL(href)) {
+        return <a {...props} rel="noopener noreferrer"></a>
+      }
+
       return <Link href={href}>
         <a {...props}></a>
       </Link>
