ff365307ffa10bccf2e0cdf5d50e8eecba9d3935
Piotr Brudny <pbrudny@gmail.com>
2022-07-08 16:11:44 +0200

    Improve Markdown display

--- components/ZaisteReactMarkdown.tsx
+++ components/ZaisteReactMarkdown.tsx
@@ -2,31 +2,28 @@ import Link from "next/link";
 import {MDXRemote} from "next-mdx-remote";
 import {MarkdownResult} from "../utils";
 
-const isExternalURL = (url:string) => {
-  if (process?.title === 'browser') {
-    return new URL(url).origin !== location.origin;
-  }
-
-  return false;
+const isExternalURL = (url: string) => {
+  return url[0] !== '/';
 }
 
-const ZaisteReactMarkdown = ({ children }: { children: MarkdownResult }) => {
+const ZaisteReactMarkdown = ({children}: { children: MarkdownResult }) => {
   return <MDXRemote
     {...children}
     components={{
-    a:({href, ...props}) => {
-      if (!href) {
-        return <a {...props}></a>
-      }
+      a: ({href, ...props}) => {
+        if (!href) {
+          return <a {...props}/>
+        }
 
-      if (isExternalURL(href)) {
-        return <a {...props} rel="noopener noreferrer"></a>
-      }
+        if (isExternalURL(href)) {
+          return <a {...props} href={href} rel={"noopener noreferrer"}/>
+        }
 
-      return <Link href={href}>
-        <a {...props}></a>
-      </Link>
-    }}}
+        return <Link href={href}>
+          <a {...props}/>
+        </Link>
+      }
+    }}
   />;
 }
 
